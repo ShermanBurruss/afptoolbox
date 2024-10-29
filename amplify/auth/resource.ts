d53d06f25d089348287983f7d5b56550e17c5585
Sherman Burruss <sherman@innovativetruckingllc.com>
2024-10-28 22:55:58 -0700

    remove defineAuth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,33 +1,33 @@
-import { defineAuth } from '@aws-amplify/backend';
+// import { defineAuth } from '@aws-amplify/backend';
 
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
-  userAttributes: {
-    givenName: {
-      mutable: true,
-      required: true,
-    },
-    familyName: {
-      mutable: true,
-      required: true,
-    },
-    "custom:scac": {
-      dataType: 'String',
-      mutable: false,
-    },
-    "custom:companyName": {
-      dataType: 'String',
-      mutable: false,
-    },
-    phoneNumber: {
-      mutable: true,
-      required: true,
-    },
-    "custom:isActive": {
-      dataType: 'Boolean',
-      mutable: false,
-    },
-  },
-});
+// export const auth = defineAuth({
+//   loginWith: {
+//     email: true,
+//   },
+//   userAttributes: {
+//     givenName: {
+//       mutable: true,
+//       required: true,
+//     },
+//     familyName: {
+//       mutable: true,
+//       required: true,
+//     },
+//     "custom:scac": {
+//       dataType: 'String',
+//       mutable: false,
+//     },
+//     "custom:companyName": {
+//       dataType: 'String',
+//       mutable: false,
+//     },
+//     phoneNumber: {
+//       mutable: true,
+//       required: true,
+//     },
+//     "custom:isActive": {
+//       dataType: 'Boolean',
+//       mutable: false,
+//     },
+//   },
+// });
